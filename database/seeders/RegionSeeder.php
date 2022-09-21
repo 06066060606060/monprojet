f6f61f8160fffd444cef1bca478b4f6133caf4b9
06066060606060 <xmicky+git@hotmail.fr>
2022-09-22 00:58:39 +0400

    seed

--- database/seeders/RegionSeeder.php
+++ database/seeders/RegionSeeder.php
@@ -5,12 +5,14 @@ namespace Database\Seeders;
 use App\Models\Regions;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Schema;
 
 class RegionSeeder extends Seeder
 {
 
     public function run()
     {
+        Schema::dropIfExists('settings');
         Regions::create(['region' => 'Nord', 'latitude' => '-20.88',  'longitude' => '55.45', 'zoom' => '14']);
         Regions::create(['region' => 'Sud', 'latitude' => '-21.31',  'longitude' => '55.50', 'zoom' => '14']);
         Regions::create(['region' => 'Ouest', 'latitude' => '-21.05',  'longitude' => '55.27', 'zoom' => '14']);
