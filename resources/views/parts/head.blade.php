105c891f924e2669375d544e7763f88c11351c72
06066060606060 <xmicky+git@hotmail.fr>
2022-09-28 16:38:32 +0400

    meta

--- resources/views/parts/head.blade.php
+++ resources/views/parts/head.blade.php
@@ -15,14 +15,14 @@
 
 <!-- Open Graph / Facebook -->
 <meta property="og:type" content="website">
-<meta property="og:url" content="https://metatags.io/">
+<meta property="og:url" content="">
 <meta property="og:title" content="GeoGraff — Océane Argelas">
 <meta property="og:description" content="A la découverte du Street-Art à la Réunion">
-<meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+<meta property="og:image" content="/img/meta.jpg">
 
 <!-- Twitter -->
 <meta property="twitter:card" content="summary_large_image">
-<meta property="twitter:url" content="https://metatags.io/">
+<meta property="twitter:url" content="">
 <meta property="twitter:title" content="GeoGraff — Océane Argelas">
 <meta property="twitter:description" content="A la découverte du Street-Art à la Réunion">
-<meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+<meta property="twitter:image" content="/img/meta.jpg">
